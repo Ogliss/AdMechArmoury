0e6aa3e3858a448430f971364175e6577a1bb916
Ogliss <randomdragon2@hotmail.com>
2019-06-22 00:45:04 +0100

    2

--- Source/AdeptusMechanicusMain/Projectile_AbilityRRY.cs
+++ Source/AdeptusMechanicusMain/Projectile_AbilityRRY.cs
@@ -1,8 +1,11 @@
-﻿using UnityEngine;
-using Verse;
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 
-namespace AbilityUser
+namespace Verse
 {
+<<<<<<< HEAD:Source/AdeptusMechanicusMain/Projectile_AbilityRRY.cs
     public class Projectile_AbilityRRY : Projectile_AbilityBase
     {
         public int TicksToImpact => ticksToImpact;
@@ -25,20 +28,30 @@ namespace AbilityUser
         {
 
         }
+=======
+    // Token: 0x02000B9E RID: 2974
+    public abstract class ResearchMod
+    {
+        // Token: 0x0600413D RID: 16701
+        public abstract void Apply();
+    }
+>>>>>>> parent of c90b7c9... update computer problems:Source/AdeptusMechanicusMain/Class2.cs
 
-        public virtual bool IsInIgnoreHediffList(Hediff hediff)
+    public class ResearchMod_SetResearchToZero : ResearchMod
+    {
+        /// <summary>
+        /// Defines a specific def. Leave this null for current def.
+        /// </summary>
+        public ResearchProjectDef def;
+        public override void Apply()
         {
-            if (hediff != null)
-                if (hediff.def != null)
-                {
-                    var compAbility = Caster.TryGetComp<CompAbilityUser>();
-                    if (compAbility != null)
-                        if (compAbility.IgnoredHediffs() != null)
-                            if (compAbility.IgnoredHediffs().Contains(hediff.def))
-                                return true;
-                }
+            var progress = (Dictionary<ResearchProjectDef, float>)typeof(ResearchManager).GetField("progress", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(Find.ResearchManager);
+            if (def == null)
+                def = Find.ResearchManager.currentProj;
 
-            return false;
+            progress[def] = 0f;
+            //For testing and spamming purposes - No real effect on standard gameplay - AND CAUSING BUGS!
+            //Find.ResearchManager.currentProj = null;
         }
     }
 }
