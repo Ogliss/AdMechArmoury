92f5d6980d3c067df0b0523dc5d2c3de6c10a066
Ogliss <randomdragon2@hotmail.com>
2020-02-21 23:54:17 +0000

    v1.5.0.1 hotfix

---
+++ 1.1/Source/PawnShields/Workers/StatWorker_Shield.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace PawnShields
+{
+    /// <summary>
+    /// Worker class specifically made to show off the snazzy stats for shields.
+    /// </summary>
+    public class StatWorker_Shield : StatWorker
+    {
+        public override bool ShouldShowFor(StatRequest req)
+        {
+            var def = req.Def as ThingDef; 
+            if (def != null && def.HasComp(typeof(CompShield)))
+                return true;
+            return false;
+        }
+        
+    }
+}
