c1d6745602bf7f6d10904b4d0e8b5cd35abe60c8
Ogliss <randomdragon2@hotmail.com>
2020-03-20 07:27:16 +0000

    disabled pysker patch

--- 1.1/Source/AdeptusMechanicusMain/Harmony/Patches/AM_ABlankPatch - Copy.cs
+++ 1.1/Source/AdeptusMechanicusMain/Harmony/Patches/AM_ABlankPatch - Copy.cs
@@ -15,6 +15,7 @@ using System.Reflection;
 
 namespace AdeptusMechanicus
 {
+	/*
     // Token: 0x02000020 RID: 32 AdeptusMechanicus.PsySilencerExt
     public class PsySilencerExt : DefModExtension
     {
@@ -57,4 +58,48 @@ namespace AdeptusMechanicus
         }
     }
 
+	// Token: 0x020003A7 RID: 935
+	public class Hediff_QXPsychicAmplifier : Hediff_ImplantWithLevel
+	{
+		// Token: 0x06001734 RID: 5940 RVA: 0x000166B5 File Offset: 0x000148B5
+		public override void PostAdd(DamageInfo? dinfo)
+		{
+			base.PostAdd(dinfo);
+			this.TryGiveAbilityOfLevel(this.level);
+		}
+
+		// Token: 0x06001735 RID: 5941 RVA: 0x000D15B4 File Offset: 0x000CF7B4
+		public override void ChangeLevel(int levelOffset)
+		{
+			if (levelOffset > 0)
+			{
+				float num = Math.Min((float)levelOffset, this.def.maxSeverity - (float)this.level);
+				int num2 = 0;
+				while ((float)num2 < num)
+				{
+					int abilityLevel = this.level + 1 + num2;
+					this.TryGiveAbilityOfLevel(abilityLevel);
+					num2++;
+				}
+			}
+			base.ChangeLevel(levelOffset);
+		}
+
+		// Token: 0x06001736 RID: 5942 RVA: 0x000D1608 File Offset: 0x000CF808
+		public void TryGiveAbilityOfLevel(int abilityLevel)
+		{
+			if (!this.pawn.abilities.abilities.Any((Ability a) => a.def.level == abilityLevel))
+			{
+				AbilityDef abilityDef = (from a in DefDatabase<AbilityDef>.AllDefs
+										 where a.level == abilityLevel
+										 select a).RandomElement<AbilityDef>();
+				this.pawn.abilities.GainAbility(abilityDef);
+				if (PawnUtility.ShouldSendNotificationAbout(this.pawn))
+				{
+					Messages.Message("PsycastLearnedFromImplant".Translate(this.pawn.Named("USER"), abilityLevel, abilityDef.LabelCap), this.pawn, MessageTypeDefOf.PositiveEvent, true);
+				}
+			}
+		}
+	}
+	*/
 }
