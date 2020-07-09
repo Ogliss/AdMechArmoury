bb803b2d99442d6fc3b97ebb70072d4e1b5431c6
Ogliss <randomdragon2@hotmail.com>
2020-07-09 23:28:24 +0100

    custom scenario bugfix

--- 1.1/Source/AdeptusMechanicusMain/Harmony/Patches/AM_Page_ScenarioLister_ScenariosInCategory_Patch.cs
+++ 1.1/Source/AdeptusMechanicusMain/Harmony/Patches/AM_Page_ScenarioLister_ScenariosInCategory_Patch.cs
@@ -19,116 +19,104 @@ namespace AdeptusMechanicus.HarmonyInstance
                 {
                     yield return scen;
                 }
-                ScenarioDef scenDef = DefDatabase<ScenarioDef>.AllDefsListForReading.Find(x => x.scenario.name == scen.name);
-            //   Log.Message("checking name: " + scen.name+ " def: "+ scenDef.defName);
-                if (scenDef.defName.Contains("OGAM_TestScenario_"))
+                if (cat == ScenarioCategory.FromDef)
                 {
-                //    Log.Message("skipping "+ scen.name);
-                    continue;
-                }
-                yield return scen;
-            }
-            yield break;
-
-        }
-        /*
-        public static IEnumerable<Scenario> ScenariosInCategoryPrefix(IEnumerable<Scenario> scenario, ScenarioCategory cat)
-        {
-            if (cat == ScenarioCategory.FromDef)
-            {
-                IEnumerable<ScenarioDef> scenarios = null;
-                scenarios = DefDatabase<ScenarioDef>.AllDefs;
-                foreach (ScenarioDef scenDef in DefDatabase<ScenarioDef>.AllDefs)
-                {
-                    if (scen.name.Contains("OG_Astartes_"))
+                    ScenarioDef scenDef = DefDatabase<ScenarioDef>.AllDefsListForReading.Find(x => x.scenario.name == scen.name);
+                    //   Log.Message("checking name: " + scen.name+ " def: "+ scenDef.defName);
+                    if (scenDef.defName.Contains("OGAM_TestScenario_"))
+                    {
+                        //    Log.Message("skipping "+ scen.name);
+                        continue;
+                    }
+                    if (scenDef.defName.Contains("OG_Astartes_"))
                     {
                         if (!SettingsHelper.latest.AllowAdeptusAstartes)
                         {
                             continue;
                         }
                     }
-                    
-                    if (scen.name.Contains("OG_Mechanicus_"))
+
+                    if (scenDef.defName.Contains("OG_Mechanicus_"))
                     {
                         if (!SettingsHelper.latest.AllowAdeptusMechanicus)
                         {
                             continue;
                         }
                     }
-                    
-                    if (scen.name.Contains("OG_Militarum_"))
+
+                    if (scenDef.defName.Contains("OG_Militarum_"))
                     {
                         if (!SettingsHelper.latest.AllowAdeptusMilitarum)
                         {
                             continue;
                         }
                     }
-                    
-                    if (scen.name.Contains("OG_Sororitas_"))
+
+                    if (scenDef.defName.Contains("OG_Sororitas_"))
                     {
                         if (!SettingsHelper.latest.AllowAdeptusSororitas)
                         {
                             continue;
                         }
                     }
-                    
-                    if (scen.name.Contains("OG_Choas_Deamons_"))
+
+                    if (scenDef.defName.Contains("OG_Choas_Deamons_"))
                     {
                         if (!SettingsHelper.latest.AllowChaosDeamons)
                         {
                             continue;
                         }
                     }
-                    
-                    if (scen.name.Contains("OG_Choas_Guard_"))
+
+                    if (scenDef.defName.Contains("OG_Choas_Guard_"))
                     {
                         if (!SettingsHelper.latest.AllowChaosGuard)
                         {
                             continue;
                         }
                     }
-                    
-                    if (scen.name.Contains("OG_Choas_Marine_"))
+
+                    if (scenDef.defName.Contains("OG_Choas_Marine_"))
                     {
                         if (!SettingsHelper.latest.AllowChaosMarine)
                         {
                             continue;
                         }
                     }
-                    
-                    if (scen.name.Contains("OG_Choas_Mechanicus_"))
+
+                    if (scenDef.defName.Contains("OG_Choas_Mechanicus_"))
                     {
                         if (!SettingsHelper.latest.AllowChaosMechanicus)
                         {
                             continue;
                         }
                     }
-                    
-                    if (scen.name.Contains("OG_Dark_Eldar_"))
+
+                    if (scenDef.defName.Contains("OG_Dark_Eldar_"))
                     {
                         if (!SettingsHelper.latest.AllowDarkEldar)
                         {
                             continue;
                         }
                     }
-                    
-                    if (scen.name.Contains("OG_Eldar_"))
+
+                    if (scenDef.defName.Contains("OG_Eldar_"))
                     {
-                        if (scen.name.Contains("Craftworld"))
+                        if (scenDef.defName.Contains("Craftworld"))
                         {
                             if (!SettingsHelper.latest.AllowEldarCraftworld)
                             {
                                 continue;
                             }
                         }
-                        if (scen.name.Contains("Exodite"))
+                        if (scenDef.defName.Contains("Exodite"))
                         {
                             if (!SettingsHelper.latest.AllowEldarExodite)
                             {
                                 continue;
                             }
                         }
-                        if (scen.name.Contains("Harlequinn"))
+                        if (scenDef.defName.Contains("Harlequinn"))
                         {
                             if (!SettingsHelper.latest.AllowEldarHarlequinn)
                             {
@@ -136,41 +124,41 @@ namespace AdeptusMechanicus.HarmonyInstance
                             }
                         }
                     }
-                    
-                    if (scen.name.Contains("OG_Tau_"))
+
+                    if (scenDef.defName.Contains("OG_Tau_"))
                     {
                         if (!SettingsHelper.latest.AllowTau)
                         {
                             continue;
                         }
                     }
-                    
-                    if (scen.name.Contains("OG_Kroot_"))
+
+                    if (scenDef.defName.Contains("OG_Kroot_"))
                     {
                         if (!SettingsHelper.latest.AllowKroot)
                         {
                             continue;
                         }
                     }
-                    
-                    if (scen.name.Contains("OG_Vespid_"))
+
+                    if (scenDef.defName.Contains("OG_Vespid_"))
                     {
                         if (!SettingsHelper.latest.AllowVespidAuxiliaries)
                         {
                             continue;
                         }
                     }
-                    
-                    if (scen.name.Contains("OG_Ork_") || scen.name.Contains("OG_Grot_"))
+
+                    if (scenDef.defName.Contains("OG_Ork_") || scenDef.defName.Contains("OG_Grot_"))
                     {
-                        if (scen.name.Contains("Tek"))
+                        if (scenDef.defName.Contains("Tek"))
                         {
                             if (!SettingsHelper.latest.AllowOrkTek)
                             {
                                 continue;
                             }
                         }
-                        if (scen.name.Contains("Feral"))
+                        if (scenDef.defName.Contains("Feral"))
                         {
                             if (!SettingsHelper.latest.AllowOrkFeral)
                             {
@@ -178,288 +166,11 @@ namespace AdeptusMechanicus.HarmonyInstance
                             }
                         }
                     }
-                    yield return scenDef.scenario;
-
                 }
+                yield return scen;
             }
-            
-            if (cat == ScenarioCategory.CustomLocal)
-            {
-                IEnumerable<Scenario> scenarios = ScenarioFiles.AllScenariosLocal;
-                foreach (Scenario scenDef in scenarios)
-                {
-                    if ((scenDef.name.Contains("OG_Astartes_")))
-                    {
-                        if (SettingsHelper.latest.AllowAdeptusAstartes)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Mechanicus_")))
-                    {
-                        if (SettingsHelper.latest.AllowAdeptusMechanicus)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Militarum_")))
-                    {
-                        if (SettingsHelper.latest.AllowAdeptusMilitarum)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Sororitas_")))
-                    {
-                        if (SettingsHelper.latest.AllowAdeptusSororitas)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Choas_Deamons_")))
-                    {
-                        if (SettingsHelper.latest.AllowChaosDeamons)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Choas_Guard_")))
-                    {
-                        if (SettingsHelper.latest.AllowChaosGuard)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Choas_Marine_")))
-                    {
-                        if (SettingsHelper.latest.AllowChaosMarine)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Choas_Mechanicus_")))
-                    {
-                        if (SettingsHelper.latest.AllowChaosMechanicus)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Dark_Eldar_")))
-                    {
-                        if (SettingsHelper.latest.AllowDarkEldar)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Eldar_")))
-                    {
-                        if ((scenDef.name.Contains("Craftworld")))
-                        {
-                            if (SettingsHelper.latest.AllowEldarCraftworld)
-                            {
-                                yield return scenDef;
-                            }
-                        }
-                        if ((scenDef.name.Contains("Exodite")))
-                        {
-                            if (SettingsHelper.latest.AllowEldarExodite)
-                            {
-                                yield return scenDef;
-                            }
-                        }
-                        if ((scenDef.name.Contains("Harlequinn")))
-                        {
-                            if (SettingsHelper.latest.AllowEldarHarlequinn)
-                            {
-                                yield return scenDef;
-                            }
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Tau_")))
-                    {
-                        if (SettingsHelper.latest.AllowTau)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Kroot_")))
-                    {
-                        if (SettingsHelper.latest.AllowKroot)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Vespid_")))
-                    {
-                        if (SettingsHelper.latest.AllowVespidAuxiliaries)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Ork_") || scenDef.name.Contains("OG_Grot_")))
-                    {
-                        if ((scenDef.name.Contains("Tek")))
-                        {
-                            if (SettingsHelper.latest.AllowOrkTek)
-                            {
-                                yield return scenDef;
-                            }
-                        }
-                        if ((scenDef.name.Contains("Feral")))
-                        {
-                            if (SettingsHelper.latest.AllowOrkFeral)
-                            {
-                                yield return scenDef;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        yield return scenDef;
-                    }
-                }
-            }
-            
-            if (cat == ScenarioCategory.SteamWorkshop)
-            {
-                IEnumerable<Scenario> scenarios = ScenarioFiles.AllScenariosWorkshop;
-                foreach (Scenario scenDef in scenarios)
-                {
-                    if ((scenDef.name.Contains("OG_Astartes_")))
-                    {
-                        if (SettingsHelper.latest.AllowAdeptusAstartes)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Mechanicus_")))
-                    {
-                        if (SettingsHelper.latest.AllowAdeptusMechanicus)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Militarum_")))
-                    {
-                        if (SettingsHelper.latest.AllowAdeptusMilitarum)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Sororitas_")))
-                    {
-                        if (SettingsHelper.latest.AllowAdeptusSororitas)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Choas_Deamons_")))
-                    {
-                        if (SettingsHelper.latest.AllowChaosDeamons)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Choas_Guard_")))
-                    {
-                        if (SettingsHelper.latest.AllowChaosGuard)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Choas_Marine_")))
-                    {
-                        if (SettingsHelper.latest.AllowChaosMarine)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Choas_Mechanicus_")))
-                    {
-                        if (SettingsHelper.latest.AllowChaosMechanicus)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Dark_Eldar_")))
-                    {
-                        if (SettingsHelper.latest.AllowDarkEldar)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Eldar_")))
-                    {
-                        if ((scenDef.name.Contains("Craftworld")))
-                        {
-                            if (SettingsHelper.latest.AllowEldarCraftworld)
-                            {
-                                yield return scenDef;
-                            }
-                        }
-                        if ((scenDef.name.Contains("Exodite")))
-                        {
-                            if (SettingsHelper.latest.AllowEldarExodite)
-                            {
-                                yield return scenDef;
-                            }
-                        }
-                        if ((scenDef.name.Contains("Harlequinn")))
-                        {
-                            if (SettingsHelper.latest.AllowEldarHarlequinn)
-                            {
-                                yield return scenDef;
-                            }
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Tau_")))
-                    {
-                        if (SettingsHelper.latest.AllowTau)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Kroot_")))
-                    {
-                        if (SettingsHelper.latest.AllowKroot)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Vespid_")))
-                    {
-                        if (SettingsHelper.latest.AllowVespidAuxiliaries)
-                        {
-                            yield return scenDef;
-                        }
-                    }
-                    else if ((scenDef.name.Contains("OG_Ork_") || scenDef.name.Contains("OG_Grot_")))
-                    {
-                        if ((scenDef.name.Contains("Tek")))
-                        {
-                            if (SettingsHelper.latest.AllowOrkTek)
-                            {
-                                yield return scenDef;
-                            }
-                        }
-                        if ((scenDef.name.Contains("Feral")))
-                        {
-                            if (SettingsHelper.latest.AllowOrkFeral)
-                            {
-                                yield return scenDef;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        yield return scenDef;
-                    }
-                }
-            }
-            
             yield break;
 
         }
-        */
     }
 }
