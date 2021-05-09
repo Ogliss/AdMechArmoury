c9fec36550b2c52badd5db54ca4a51d09d33373e
Ogliss <randomdragon2@hotmail.com>
2021-05-09 02:48:23 +0100

    removed logg

--- 1.1/Source/AdeptusMechanicusMain/Harmony/Patches/PawnBioAndNameGenerator_FillBackstorySlotShuffled_Controller_Patch.cs
+++ 1.1/Source/AdeptusMechanicusMain/Harmony/Patches/PawnBioAndNameGenerator_FillBackstorySlotShuffled_Controller_Patch.cs
@@ -18,12 +18,9 @@ namespace AdeptusMechanicus.HarmonyInstance
         [HarmonyPrefix]
         public static bool Prefix(Pawn pawn, BackstorySlot slot, ref Backstory backstory, Backstory backstoryOtherSlot, List<BackstoryCategoryFilter> backstoryCategories, FactionDef factionType)
         {
-            Log.Message("PawnBioAndNameGenerator_FillBackstorySlotShuffled_Controller_Patch For: "+ pawn +" slot: " +slot+ " backstoryOtherSlot: "+ backstoryOtherSlot+ " backstoryCategories: "+ backstoryCategories.Count + " factionType: "+ factionType);
             bool act = pawn.def.modContentPack != null && pawn.def.modContentPack.Name.Contains("Adeptus Mechanicus");
-            Log.Message("modContentPack: "+ act);
             if (act || pawn.def.defName.StartsWith("OG_") || pawn.kindDef.defName.StartsWith("OG_") || pawn.kindDef.defName.Contains("_OG_"))
             {
-                Log.Message("FillBackstorySlotShuffled_Controller act");
                 FillBackstorySlotShuffled(pawn, slot, ref backstory, backstoryOtherSlot, backstoryCategories, factionType);
                 return false;
             }
@@ -31,12 +28,9 @@ namespace AdeptusMechanicus.HarmonyInstance
         }
         public static bool FillBackstorySlotShuffled(Pawn pawn, BackstorySlot slot, ref Backstory backstory, Backstory backstoryOtherSlot, List<BackstoryCategoryFilter> backstoryCategories, FactionDef factionType)
         {
-            Log.Message("FillBackstorySlotShuffled act 1");
             bool act = pawn.def.modContentPack != null && pawn.def.modContentPack.Name.Contains("Adeptus Mechanicus");
-            Log.Message("FillBackstorySlotShuffled act 2");
             if (act || pawn.def.defName.StartsWith("OG_") || pawn.kindDef.defName.StartsWith("OG_") || pawn.kindDef.defName.Contains("_OG_"))
             {
-                Log.Message("FillBackstorySlotShuffled acting 1");
                 BackstoryCategoryFilter backstoryCategoryFilter = backstoryCategories.RandomElementByWeight((BackstoryCategoryFilter c) => c.commonality);
                 if (backstoryCategoryFilter == null)
                 {
